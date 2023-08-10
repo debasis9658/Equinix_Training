cfd1f3723d83a2ba1e7edaa643fea60aa56835af
Debasish Biswal <debasisjkbk@gmail.com>
2023-08-10 13:12:04 +0530

    Completed multiplication without using operator

--- problemSolving/multiplication.cpp
+++ problemSolving/multiplication.cpp
@@ -53,5 +53,54 @@ int main()
         }
         cout<<endl;
     }
+    
+    int no_of_zeros_added = m - 1;
+    
+    for(int i = m - 1; i >= 0; i--){
+        int back = no_of_zeros_added;
+        int front = (m + n) - back - ans[i].size();
+        while(back > 0){
+            ans[i].push_back(0);
+            back--;
+        }
+        while(front > 0){
+            ans[i].insert(ans[i].begin(), 0);
+            front--;
+        }
+
+        no_of_zeros_added--;
+
+    }
+
+    for(int i = 0; i < ans.size(); i++){
+        cout<<ans[i].size()<<" ";
+    }
+    cout<<endl;
+    cout<<endl;
+    vector<int> fs;
+    int s = 0, c = 0;
+    for(int i = (m + n - 1); i >= 0; i--){
+        int sum = c;
+        for(int j = 0; j < m; j++){
+            sum += ans[j][i];
+        }
+        if(sum >= 10){
+           fs.insert(fs.begin(), sum % 10);
+           c = sum / 10;
+        }
+        else{
+            fs.insert(fs.begin(), sum);
+            c = 0;
+        }
+    }
+    if(c > 0){
+        fs.insert(fs.begin(), c);
+    }
+    
+    int res = 0;
+    for(int i = 0; i < fs.size(); i++){
+        res = res * 10 + fs[i];
+    }
+    cout<<res<<endl;
     return 0;
 }
